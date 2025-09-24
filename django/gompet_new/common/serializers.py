d1a52e19e427e873b515a688896502eb80dd3d83
SchizZzo <83632210+SchizZzo@users.noreply.github.com>
2025-09-24 09:50:06 +0200

    Remove unused import from serializers

--- django/gompet_new/common/serializers.py
+++ django/gompet_new/common/serializers.py
@@ -1,16 +1,32 @@
 
 from rest_framework import serializers
 from django.contrib.contenttypes.models import ContentType
-from django.utils import timezone
 from common.models import Comment, Reaction, ReactionType
 
 
+class ContentTypeRelatedField(serializers.PrimaryKeyRelatedField):
+    """Field accepting ContentType by PK or "app_label.model" string."""
+
+    def __init__(self, **kwargs):
+        super().__init__(queryset=ContentType.objects.all(), **kwargs)
+
+    def to_internal_value(self, data):
+        if isinstance(data, str) and "." in data:
+            app_label, model = data.split(".", 1)
+            app_label = app_label.lower()
+            model = model.lower()
+            try:
+                return self.get_queryset().get(
+                    app_label=app_label, model=model
+                )
+            except ContentType.DoesNotExist:
+                self.fail("does_not_exist", pk_value=data)
+        return super().to_internal_value(data)
+
 
 class CommentSerializer(serializers.ModelSerializer):
-    # wybieramy ContentType po its PK
-    content_type = serializers.PrimaryKeyRelatedField(
-        queryset=ContentType.objects.all()
-    )
+    # wybieramy ContentType po its PK lub etykiecie "app_label.model"
+    content_type = ContentTypeRelatedField()
     # użytkownik z request.user (ukryte pole)
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
@@ -38,9 +54,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class ReactionSerializer(serializers.ModelSerializer):
-    reactable_type = serializers.PrimaryKeyRelatedField(
-        queryset=ContentType.objects.all()
-    )
+    reactable_type = ContentTypeRelatedField()
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
