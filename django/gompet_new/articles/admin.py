921c60a252e00f3292a8a300afe09d33f7783cc5
Dawid Michałowicz <dawid.michalowicz@hotmail.com>
2025-12-23 21:23:30 +0100

    add-kategorie-do-admin

--- django/gompet_new/articles/admin.py
+++ django/gompet_new/articles/admin.py
@@ -7,12 +7,16 @@ from .models import Article, ArticleCategory
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "slug", "author", "created_at", "deleted_at")
+    list_display = ("id", "title", "get_categories", "slug", "author", "created_at", "deleted_at")
     list_filter = ("deleted_at", "created_at", "author")
     search_fields = ("title", "content", "author__username", "author__email")
     prepopulated_fields = {"slug": ("title",)}
     ordering = ("-created_at",)
     raw_id_fields = ("author",)
+
+    def get_categories(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    get_categories.short_description = "Categories"
     readonly_fields = ("created_at", "updated_at", "deleted_at")
     fieldsets = (
         (None, {
