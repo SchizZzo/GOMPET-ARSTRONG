f317acea4470766d9d26f6ae7b3cac3b7b2321c7
SchizZzo <83632210+SchizZzo@users.noreply.github.com>
2026-01-08 21:48:24 +0100

    Expand role permissions across apps

--- django/gompet_new/users/apps.py
+++ django/gompet_new/users/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+from .role_permissions import ensure_member_role_groups
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self) -> None:
+        post_migrate.connect(
+            create_member_role_groups,
+            sender=self,
+            dispatch_uid="users.ensure_member_role_groups",
+        )
+
+
+def create_member_role_groups(**kwargs) -> None:
+    ensure_member_role_groups(using=kwargs.get("using"))
