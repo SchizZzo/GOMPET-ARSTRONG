9cdc976a10225bbf8e781625e82abe9a1bf36c2e
Dawid Michałowicz <dawid.michalowicz@hotmail.com>
2026-02-16 12:10:48 +0100

    Follow

--- django/gompet_new/common/admin.py
+++ django/gompet_new/common/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Comment, Reaction, Notification
+from .models import Comment, Reaction, Notification, Follow
 
 admin.site.register(Comment)
 admin.site.register(Reaction)
 admin.site.register(Notification)
+admin.site.register(Follow)
